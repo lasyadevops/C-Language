7acdeaa1ceb236cec85406aaa83a02be87df0fe6
lasyadevops <bommalasya29@gmail.com>
2023-07-11 18:21:02 +0530

    fsiuod

---
+++ array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int number[8]={1,3,5,6,7,8,3,10};
+	system("clear");
+	number[3]=97;
+	number[0]=56;
+		for(int b=0;b<=9;b++)
+		{		printf("%d ", number[b] );
+}
+}
+
