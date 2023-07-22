84ed2d1c3763f1c858bac053f6cfa4c05aea0b3c
lasyadevops <bommalasya29@gmail.com>
2023-07-22 11:06:02 +0530

    leujoa

---
+++ stringfun.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void displaystring(char c[]);
+int main()
+{
+	char c[50];
+	system("clear");
+	printf("\n enter any string :");
+	fgets(c,sizeof(c),stdin);
+	displaystring(c);
+	return 0 ;
+}
+void displaystring(char c[])
+{
+	printf("\n string output is : ");
+	puts(c);
+
+
+
+
+}
