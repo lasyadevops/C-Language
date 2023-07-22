0c9457f239c3be65860d15da16c7aba8498b0804
lasyadevops <bommalasya29@gmail.com>
2023-07-22 18:49:32 +0530

    fyuh

---
+++ partstringf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{
+        char string[30]="Amma Bramha Cnanda ";
+        system("clear");
+          printf("%s\n\n\n", strchr(string,'C') );
+
+
+
+
+}
