46abdd2ae76482790f8a5eb145b393ccb33befd2
lasyadevops <bommalasya29@gmail.com>
2023-07-22 11:23:37 +0530

    djosia

---
+++ stringfun2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+        char c[50];
+        system("clear");
+	printf("\n enter any string : ");
+	scanf("%s",c);
+        printf("\n %s is given string \n\n\n",c);
+}
